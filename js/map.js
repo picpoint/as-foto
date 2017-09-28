c04afbced0592aa5f59eb7f2dcc39a0fe721092e
picpoint <picpoint@yandex.ru>
2017-09-28 10:19:44 +0300

    Работа над блоком КОНТАКТЫ

---
+++ js/map.js
@@ -0,0 +1,5 @@
+<script type="text/javascript"
+	charset="utf-8"
+	async
+	src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3649edcb9e65c5dff3b68b4ea911261f2e6001f5822736e839bfd84136734575&amp;width=1149&amp;height=403&amp;lang=ru_RU&amp;scroll=true">
+</script>
